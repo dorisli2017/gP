1a2c7a64f48053bb263652f5c21167098e3362f8
ailixiaod@gmail.com <ailixiaod@gmail.com>
2018-06-25 14:43:17 +0200

    add dual graph

--- fileConverter/fileConvert.cpp
+++ fileConverter/fileConvert.cpp
@@ -4,31 +4,33 @@
 *  Created on: Jun 25, 2018
 *      Author: ping
 */
-#include <iostream>     // std::cout
-#include <fstream>      // std::ifstream
-#include <cstdlib>
-#include <string.h>
-using namespace std;
+#include "fileConvert.h"
 int main(int argc, char *argv[]){
-    const char s[2] = " ";
-	ifstream fp;
-	ofstream outFP;
 	fp.open(argv[1],std::ios::in);
-	string b = ".p";
-	b =string(argv[1])+ b;
 	if(!fp.is_open()){
 		perror("read file fails");
 		exit(EXIT_FAILURE);
 	}
-	outFP.open(b.c_str(),std:: ios::out);
+	if(argc > 2){
+		outFP.open((string(argv[1])+ p).c_str(),std:: ios::out);
+		primary();
+	}
+	else{
+		outFP.open((string(argv[1])+ d).c_str(),std:: ios::out);
+		dual();
+	}
+	outFP.close();
+	fp.close();
+
+};
+
+void primary(){
 	if(!outFP.is_open()){
 		perror("write file fails");
 				exit(EXIT_FAILURE);
 	}
 	// strat read
-	string buff;
 	char head;
-	int numCs;
 	getline(fp,buff);
 	// Get the p line
 	while(!fp.eof()){
@@ -40,8 +42,9 @@ int main(int argc, char *argv[]){
 			char* str = strdup(buff.c_str());
 	    	strtok(str, s);
 			strtok(NULL, s);
+			numVs = atoi(strtok(NULL, s));
 			numCs = atoi(strtok(NULL, s));
-			outFP<< numCs << ' '<< strtok(NULL, s)<< endl;
+			outFP<< numCs << ' '<< numVs<< endl;
 			break;
 		}
 	  getline(fp,buff);
@@ -75,9 +78,81 @@ int main(int argc, char *argv[]){
 			token = strtok(NULL, s);
 		 }
 	}
+}
 
-	outFP.close();
-	fp.close();
+void dual(){
+	if(!outFP.is_open()){
+			perror("write file fails");
+					exit(EXIT_FAILURE);
+		}
+	char head;
+	   	getline(fp,buff);
+	   	// Get the p line
+	   	while(!fp.eof()){
+			//cout<<buff<<endl;
+			//todo:parseLine
+	   		if(buff.empty()) break;
+			head =buff.at(0);
+			if(head == 'p'){
+				char* str = strdup(buff.c_str());
+		    	strtok(str, s);
+				strtok(NULL, s);
+				numVs = atoi(strtok(NULL, s));
+				numCs = atoi(strtok(NULL, s));
+				Cs = new vector<int>[numVs+1];
+				break;
+			}
+		  getline(fp,buff);
+		}
 
-};
+	   	// Get the clauses
+	   	int index = 0;
+	   	int line = 0;
+	   	while(!fp.eof() && line < numCs){
+	   		index++;
+			getline(fp,buff);
+			if(buff.empty()) break;
+			parseLine(buff, index);
+			line++;
+	   	}
+	   	initialAssignment();
+}
 
+void parseLine(string line,int indexC){
+	char* str = strdup(line.c_str());
+    const char s[2] = " ";
+    int lit;
+    char* token = strtok(str, s);
+    while(token != NULL){
+		if(*token== '-'){
+			lit = atoi(token);
+			Cs[-lit].push_back(indexC);
+			token = strtok(NULL, s);
+			continue;
+		}
+		if(*token == '0'){
+		    return;
+		}
+		lit = atoi(token);
+		Cs[lit].push_back(indexC);
+		token = strtok(NULL, s);
+    }
+	perror("a clause line does not terminates");
+	exit(EXIT_FAILURE);
+}
+
+void initialAssignment(){
+	int size = 0;
+	for(int j = 0; j < numVs+1; j++){
+		if(Cs[j].size() == 0) continue;
+		size++;
+	}
+	outFP<< size << ' '<< numCs<< endl;
+	for(int j = 0; j < numVs+1; j++){
+		if(Cs[j].size() == 0) continue;
+		for (std::vector<int>::const_iterator i = Cs[j].begin(); i != Cs[j].end(); ++i){
+			outFP<<*i << " ";
+		}
+		outFP<<endl;
+	}
+}
